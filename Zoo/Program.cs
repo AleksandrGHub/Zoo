3f7fbb5a709ee49ad2931bc3a1bf51efa818de9a
Matteo <t8m39rv3zy@gmail.com>
2023-10-08 20:12:31 +0300

    Добавьте файлы проекта.

---
+++ Zoo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Zoo
+    {
+
+    }
+
+    class Aviary
+    {
+
+    }
+
+    class Animal
+    {
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public string Sound { get; private set; }
+        public string Gender { get; private set; }
+    }
+}
